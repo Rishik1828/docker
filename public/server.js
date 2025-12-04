28bd8b970114ea2908db254c961250ee7be05200
Rishik1828 <rishiksai66@gmail.com>
2025-12-04 16:14:51 +0530

    Create server.js

---
+++ public/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let data = [];
+if (fs.existsSync("data.json")) {
+    data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+}
+
+app.post("/submit", (req, res) => {
+    const message = req.body.message;
+
+    data.push({ message, time: new Date().toISOString() });
+
+    fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
+
+    res.send("<h1>Saved!</h1><a href='/'>Go Back</a>");
+});
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
